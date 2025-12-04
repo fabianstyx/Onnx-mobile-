0a87bc32c1d0e724cc47179ac6bea0d11e9ff9fb
Fabianstyx <150749430+fabianstyx@users.noreply.github.com>
2025-12-04 00:51:45 -0600

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,14 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.5.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+    }
+}
+plugins {
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}

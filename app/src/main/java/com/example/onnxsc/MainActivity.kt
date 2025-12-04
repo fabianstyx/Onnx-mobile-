e6b0cf67656e9ca034ccaf198e28ff87ae6ed756
Fabian <kdjizbh187@gmail.com>
2025-12-04 04:23:04 -0600

    Fix MainActivity.kt location and confirm manifest

---
+++ app/src/main/java/com/example/onnxsc/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.onnxsc
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(android.R.layout.simple_list_item_1) // layout temporal
+    }
+}
